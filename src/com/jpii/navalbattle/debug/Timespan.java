58defe524d655bfe73727d86ee5728d7a25dd637
Mr. Pyram <pyram3d@gmail.com>
2012-08-24 06:41:03 -0500

    Added Timespan / Stopwatch class.
    Other stuff not ready for uploading.

---
+++ src/com/jpii/navalbattle/debug/Timespan.java
@@ -0,0 +1,37 @@
+package com.jpii.navalbattle.debug;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Stopwatch class, used for measuring how long something took.
+ * @author MKirkby
+ *
+ */
+public class Timespan {
+	
+	private long startTime = 0;
+	
+	/**
+	 * Initialises the stopwatch class.
+	 */
+	public Timespan() {
+		
+	}
+	
+	/**
+	 * Starts the stopwatch.
+	 */
+	public void start()	{
+		startTime = System.nanoTime();
+	}
+	
+	/**
+	 * Stops and resets the stopwatch, and returns the duration, in milliseconds.
+	 * @return
+	 */
+	public long stop() {
+		long duration = System.nanoTime() - startTime;
+		startTime = 0;
+		return TimeUnit.MILLISECONDS.convert(duration, TimeUnit.NANOSECONDS);
+	}
+}
